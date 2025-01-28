7d2483cb700d5fb41f06554f3f2dff2d4f328489
root <root@DESKTOP-7TV10FL.>
2025-01-28 06:09:02 +0100

    ntg

--- fdf.c
+++ fdf.c
@@ -1,18 +1,15 @@
 #include "fdf.h"
 
-int     get_width(char* file_name, fdf* data)
+int check_extention(char *file_name)
 {
-    int fd = open (file_name, O_RDONLY);
-    if (fd < 0) 
-        return 0;
-    char *line = get_next_line(fd);
-    if (!line)
-        return (close(fd), 0);
-    data->width = ft_strlen(line);
-    return (free(line),close(fd),1);
+    size_t name_len = ft_strlen(file_name) - 4;
+    *file_name += name_len;
+    if (!ft_strncmp(file_name, ".fdf", 4))
+        return (1);
+    return (0);
 }
 
-int     get_height(char* file_name, fdf* data)
+int     get_two_dimentions(char* file_name, t_fdf* data)
 {
     int count = 0;
     int fd = open (file_name, O_RDONLY);
@@ -20,90 +17,35 @@ int     get_height(char* file_name, fdf* data)
         return 0;
     char *line = get_next_line(fd);
     if (!line)
-        return 0;
+        return (close(fd), 0);
+    data->width = ft_strlen(line);
     count++;
     free(line);
     while (line != NULL)
     {
         line = get_next_line(fd);
         if (!line)
-            return 0;
+            return (close(fd), 0);
         count++;
         free(line);
     }
     data->height = count;
-    return 1;
-}
-
-int     allocate_int_matrix(fdf* data)
-{
-    data->z_matrix = malloc (sizeof(int *) * (data->height));
-    if (!data->z_matrix)
-        return 0;
-    int i = 0;
-    while (i < data->width)
-    {
-        data->z_matrix[i] = malloc(sizeof(int) * (data->width));
-        i++;
-    }
-    return 1;
-}
-
-void    populate_line(fdf *data, char** matrix, int i)
-{
-    int j = 0;
-    while (j < data->width)
-    {
-        data->z_matrix[i][j] = ft_atoi(matrix[j]);
-        j++;
-    }
-}
-
-int     populate_matrix(char* file_name, fdf * data)
-{
-    char * line;
-    char **matrix;
-    int fd = open (file_name, O_RDONLY);
-    if(fd < 0)
-        return 1;
-    int i = 0;
-    int j = 0;
-    while (i < data->height)
-    {
-        line = get_next_line(fd);
-        if (!line)
-            return 1;
-        matrix = ft_split(line, 32);
-        if (!matrix)
-            return 1;
-        free(line);
-        populate_line(data, matrix, i);
-            // data->z_matrix[i][j] = (ft_atoi(matrix[j]));
-        free(matrix);
-        i++;
-    }
-    return 0;
+    return (close(fd), 1);
 }
 
 int     main(int argc, char** argv)
 {
-    fdf* data;
-    data = malloc(sizeof(fdf));
+    t_fdf* data;
+
+    if (argc != 2)
+        return (ft_printf("invalid number of arguments.\n"), (1));
+    if (!check_extention(argv[1]))
+        return (ft_printf("wrong file extension.\n"), (1));
+    data = malloc(sizeof(t_fdf));
     if(!data)
-        return 1;
-    if (get_height(argv[1], data) < 0)
+        return (ft_printf("error.\n"), (1));
+    if (!get_two_dimentions(argv[1], data))
         return (free(data), 1);
-    if (get_width(argv[1], data) < 0)
-        return (free(data), 1);
-    if (allocate_int_matrix(data) < 0)
-        return (free(data), 1);
-    //should free the matrix too from now on 
-    data->mlx_ptr = mlx_init();
-    if (!data->mlx_ptr)
-        return (/**/free(data), 1);
-    data->win_ptr = mlx_new_window(data->mlx_ptr, 600, 800, "fdf");
-    if (!data->win_ptr)
-        return (/**/free(data), 1);
-    if (!populate_matrix(argv[1], data))
-        return (/**/free(data), 1);
+    data->line[0]->y;
+    return (0);
 }
--- fdf.h
+++ fdf.h
@@ -5,21 +5,26 @@
 #include "get_next_line/get_next_line.h"
 #include "printf_bonus_norm/ft_printf.h" 
 
-#include "minilibx/mlx.h"
 #include <fcntl.h>
 #include <stdio.h>
 #include <unistd.h>
 
-void	*mlx_init();
-void	*mlx_new_window(void *mlx_ptr, int size_x, int size_y, char *title);
 
-typedef struct
+typedef struct s_map
+{
+	int z;
+	int x;
+	int max_x;//len of line y
+	char *color;
+
+}	t_map;
+
+typedef struct s_fdf
 {
 	int		width;
 	int		height;
-	int		**z_matrix;
-	void	*mlx_ptr;
-	void	*win_ptr;
-}   fdf;
+	t_map 	**line;
+
+}   t_fdf;
 
 #endif
